9449a061521b5a59c5016c25e23192655022629e
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-02-26 11:56:27 +0530

    Initial Commit

---
+++ ReusableViews_Feb2024/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReusableViews_Feb2024
+//
+//  Created by mac on 26/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReusableViews_Feb2024/ReusableViews_Feb2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableViews_Feb2024App.swift
+//  ReusableViews_Feb2024
+//
+//  Created by mac on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ReusableViews_Feb2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
